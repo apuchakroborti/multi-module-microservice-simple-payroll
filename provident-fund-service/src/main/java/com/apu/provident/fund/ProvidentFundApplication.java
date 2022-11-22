ca6493368517de089dc1b3c862074b7fb373ce2c
Apu Kumar Chakroborti <apu.chakroborti@tigerit.com>
2022-11-22 15:04:35 +0600

    eureka client dependency added

--- provident-fund-service/src/main/java/com/apu/provident/fund/ProvidentFundApplication.java
+++ provident-fund-service/src/main/java/com/apu/provident/fund/ProvidentFundApplication.java
@@ -2,8 +2,10 @@ package com.apu.provident.fund;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProvidentFundApplication {
 
 	public static void main(String[] args) {
